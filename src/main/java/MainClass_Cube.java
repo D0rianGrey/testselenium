c7252aef4df96cb101f9083e1a0618be8818e1d8
Eugene Vakerin <32601673+D0rianGrey@users.noreply.github.com>
2018-12-30 20:13:43 +0200

    division of two main classes

--- src/main/java/MainClass_Cube.java
+++ src/main/java/MainClass_Cube.java
@@ -2,19 +2,14 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class MainClass {
+public class MainClass_Cube {
 
     public static void main(String[] args) {
 
         // FireFox
 
-        //Cube
 
         System.setProperty("webdriver.gecko.driver", "C:\\Users\\utyfd\\IdeaProjects\\testselenium\\drivers\\geckodriver.exe");
-
-        //Home
-
-        System.setProperty("webdriver.gecko.driver", "C:\\Users\\User\\Desktop\\testselenium\\drivers\\geckodriver.exe");
 
         WebDriver driver = new FirefoxDriver();
 
@@ -22,13 +17,8 @@ public class MainClass {
 
         // Chrome
 
-        //Cube
 
         System.setProperty("webdriver.gecko.driver", "C:\\Users\\utyfd\\IdeaProjects\\testselenium\\drivers\\chromedriver.exe");
-
-        //Home
-
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\Desktop\\testselenium\\drivers\\chromedriver.exe");
 
         WebDriver driver2 = new ChromeDriver();
 
