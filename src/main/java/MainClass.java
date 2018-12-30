2144e1e023efc85b09119ad2a02887e990f76f18
Eugene Vakerin <32601673+D0rianGrey@users.noreply.github.com>
2018-12-30 19:46:27 +0200

    Initial commit

---
+++ src/main/java/MainClass.java
@@ -0,0 +1,30 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+
+        // FireFox
+
+        System.setProperty("webdriver.gecko.driver", "C:\\Users\\User\\Desktop\\testselenium\\drivers\\geckodriver.exe");
+
+        WebDriver driver = new FirefoxDriver();
+
+        driver.get("https://www.seleniumhq.org/");
+
+        // Chrome
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\Desktop\\testselenium\\drivers\\chromedriver.exe");
+
+        WebDriver driver2 = new ChromeDriver();
+
+        driver2.get("https://www.google.com.ua/");
+
+
+
+
+    }
+
+}
