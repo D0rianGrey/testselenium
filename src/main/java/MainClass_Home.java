3592c43636c2671590e482dd3c28d63a377619ea
Eugene Vakerin <32601673+D0rianGrey@users.noreply.github.com>
2019-01-07 16:47:38 +0200

    last

--- src/main/java/MainClass_Home.java
+++ src/main/java/MainClass_Home.java
@@ -1,6 +1,10 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
 
 public class MainClass_Home {
 
@@ -25,12 +29,31 @@ public class MainClass_Home {
 
         WebDriver driver = new ChromeDriver();
 
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
         driver.manage().window().setSize((new Dimension(900,500)));
+
+        driver.get("http://en.wikipedia.org");
+
+        WebElement link = driver.findElement(By.linkText("Log in"));
+        WebElement link2 = driver.findElement(By.partialLinkText("Donate"));
+        WebElement searchField = driver.findElement(By.name("go"));
+        WebElement searchButton = driver.findElement(By.className("searchButton"));
+        WebElement searchform = driver.findElement(By.id("searchform"));
+        WebElement input = driver.findElement(By.tagName("input"));
+        WebElement element = driver.findElement(By.cssSelector("#searchInput"));
+        WebElement logo= driver.findElement(By.xpath("//a[@title='Visit the main page']"));
+
+
+
+        driver.quit();
+
+
         // driver.manage().window().maximize();
 
 
 
-        driver.get("https://www.google.com.ua/");
+        /*driver.get("https://www.google.com.ua/");
         driver.navigate().to("http://asiaworld.cn.ua");
 
         driver.navigate().back();
@@ -38,7 +61,7 @@ public class MainClass_Home {
         driver.navigate().refresh();
 
         System.out.println(driver.getTitle());
-        System.out.println(driver.getCurrentUrl());
+        System.out.println(driver.getCurrentUrl()); */
 
         driver.quit();
 
