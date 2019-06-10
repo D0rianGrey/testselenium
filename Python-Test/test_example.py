fa80e35508c0e910c5ef9ed42977aa8de274383e
Eugene <utyfdfrthby08@gmail.com>
2019-06-10 23:20:51 +0300

    1

--- Python-Test/test_example.py
+++ Python-Test/test_example.py
@@ -1,7 +1,5 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 
 @pytest.fixture
@@ -14,6 +12,6 @@ def driver(request):
 def test_example(driver):
     driver.get("http://www.google.com.ua")
     driver.find_element_by_name("q").send_keys("webdriver")
-    driver.find_element_by_name("btnG").click()
-    WebDriverWait(driver, 10).until(EC.title_is("webdriver - Поиск в Google"))
+    driver.find_element_by_name('btnK').click()
+
 
